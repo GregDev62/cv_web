0a8ac82e9b4f3e23492ca6d1d2f7fa6486a1662e
GregDev62 <duquenne.gregoire@gmail.com>
2021-03-02 16:46:33 +0100

    init

---
+++ src/app/MainPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainPage = () => {
+  return <button>lèche mes boules</button>;
+};
+
+export default MainPage;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  entry: `${__dirname}/src/app/index.js`,
+  output: {
+    filename: 'transformed.js',
+    path: `${__dirname}/build`
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: `${__dirname}/src/app/index.html`,
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
+};
